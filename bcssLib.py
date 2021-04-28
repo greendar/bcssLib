c0c4bf1ec5e3c4d05ef052296cd92e4c5f8764da
Mr Green <dwgreen@hwdsb.on.ca>
2021-04-28 09:49:47 -0400

    created returnChoiceFromListOfObjects

--- bcssLib.py
+++ bcssLib.py
@@ -6,10 +6,9 @@ def returnChoiceFromList(listIn):
     Note that to account for 0 indexing we are subtracting 1 when we assign the
     variable choice
     """
-
     count = 1
     for item in listIn:
-        capItem = item.capitalize()  # what if it is not a string?
+        capItem = item.capitalize()  # what if it is not a string?/ capitalization nonPermanent
         print(f'{count}. ', capItem)
         count += 1
     getChoice = int(input('\nWhich would you like to choose?: '))
@@ -17,9 +16,34 @@ def returnChoiceFromList(listIn):
     listIn.remove(choice)
     return choice, listIn
 
+def returnChoiceFromListOfObjects(objListIn):
+    """This function will return a choice from a list of objects and the remaining
+    list of objects with the choice removed.
+
+    It is assumed that the objects have a name attribute.
+    """
+    nameStrList = []
+    for object in objListIn:
+        nameStrList.append(object.name)
+
+    choiceName, remList = returnChoiceFromList(nameStrList)
+    # remove object that was chosen
+    for object in objListIn:
+        if object.name == choiceName:
+            objListIn.remove(object)
+
+    return choiceName, objListIn
+
 
 
 if __name__ == "__main__":
-    myList = ['Apple', 'Carrot', 'dragon']
-    a, myList = returnChoiceFromList(myList)
-    print(a, myList)
+    from delMe import *
+    a = Student('Albert', 17, 2345)
+    b = Student('Betty', 15, 5467)
+    c = Student('Charles', 14, 6587)
+    studentList = [a, b, c]
+    choice, listOut = returnChoiceFromListOfObjects(studentList)
+    print(choice)
+    print()
+    for student in listOut:
+        print(student)
---
+++ delMe.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, age, studNumber):
+        self.name = name
+        self.age = age
+        self.studNumber = studNumber
+
+
+    def __str__(self):
+        return f"Name {self.name}, Age: {self.age}, Student Number: {self.studNumber}"
